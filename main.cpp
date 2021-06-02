23e4a6017ccc1e0383ef63ead0be1ba72f026089
MCLEANS <jackagwaya@gmail.com>
2021-06-02 22:19:40 +0300

    Added Upcoming tasks

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 #include <task.h>
 #include <portmacro.h>
 
+/**
+ *  To Do : Read Angle data from accelerometer
+ *          Create task to perform data reading.
+ *          Create Serial output to flash out data
+ */
+
 custom_libraries::clock_config system_clock;
 
 int main(void) {
   system_clock.initialize();
-  
+
   while(1){}
 }
