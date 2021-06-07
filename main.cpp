3db7440cb3421572372b75af9ebb1c2b455e826b
MCLEANS <jackagwaya@gmail.com>
2021-06-07 21:25:10 +0300

    created base_servo object

--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@
 #include <task.h>
 #include <portmacro.h>
 
+#include <MG996R.h>
+
+custom_libraries::MG996R base_servo(TIM4,
+                                    custom_libraries::channel1,
+                                    GPIOB,
+                                    6,
+                                    custom_libraries::AF2
+                                    );
+
 /**
  *  To Do : Read Angle data from accelerometer
  *          Create task to perform data reading.
  *          Create Serial output to flash out data
  */
+
 
 custom_libraries::clock_config system_clock;
 
